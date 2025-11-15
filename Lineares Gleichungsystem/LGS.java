30130f6f1c3762243420a673a269aaa197ea179d
duxn2901 <vdb2901k55@gmail.com>
2025-11-15 21:48:13 +0100

    5

--- Lineares Gleichungsystem/LGS.java
+++ Lineares Gleichungsystem/LGS.java
@@ -4,6 +4,7 @@ public class LGS {
     public static void main(String[] args) {
         erstellen();
         addieren(2, 0, -2);
+        loesen();
         ausgeben();
     }
 
@@ -23,9 +24,9 @@ public class LGS {
 
     }
 
-    public static void addieren(int a, int b, double x) {
+    public static void addieren(int zu, int von, double x) {
         for (int i = 0; i < 4; i++) {
-            matrix[a][i] += matrix[b][i] * x;
+            matrix[zu][i] += matrix[von][i] * x;
         }
     }
 
@@ -37,5 +38,36 @@ public class LGS {
             }
             System.out.println();
         }
+    }
+
+    public static void multiplizieren(int zeile, double x) {
+        for (int i = 0; i < 4; i++) {
+            matrix[zeile][i] *= x;
+        }
+    }
+
+    public static void loesen() {
+        //1.spalt: [1][0] und [2][0] nullen
+        addieren(1, 0, -1*(matrix[1][0]/matrix[0][0]));
+        addieren(2, 0, -1*(matrix[2][0]/matrix[0][0]));
+
+        //2.spalt: [2][1] nullen
+        addieren(2, 1, -1*(matrix[2][1]/matrix[1][1]));
+
+        //[2][2] 1 machen
+        multiplizieren(2, 1/matrix[2][2]);
+
+        //3.spalt: [0][2] und [1][2] nullen
+        addieren(0, 2, -1*(matrix[0][2]));
+        addieren(1, 2, -1*(matrix[1][2]));
+
+        //[1][1] 1 machen
+        multiplizieren(1, 1/matrix[1][1]);
+
+        //2.spalt [0][1] nullen
+        addieren(0, 1, -1*(matrix[0][1]));
+
+        //[0][0] 1 machen
+        multiplizieren(0, 1/matrix[0][0]);
     }
 }
