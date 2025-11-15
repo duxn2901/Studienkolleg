3f6697fbf0804f6c54f30151edc7b6f42e550440
duxn2901 <vdb2901k55@gmail.com>
2025-11-15 21:05:40 +0100

    2

--- Lineares Gleichungsystem/LGS.java
+++ Lineares Gleichungsystem/LGS.java
@@ -1,6 +1,24 @@
 public class LGS {
     
-    double[][] matrix = new double[3][4];
+    static double[][] matrix = new double[3][4];
     public static void main(String[] args) {
+        erstellen();
+        System.out.println(matrix);
+    }
+
+    public static void erstellen() {
+        matrix[0][0] = 1;
+        matrix[0][1] = 1;
+        matrix[0][2] = 1;
+        matrix[0][3] = 6;
+        matrix[1][0] = 1;
+        matrix[1][1] = -2;
+        matrix[1][2] = 2;
+        matrix[1][3] = 3;
+        matrix[2][0] = 2;
+        matrix[2][1] = 1;
+        matrix[2][2] = -1;
+        matrix[2][3] = 1;
+
     }
 }
