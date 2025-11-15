ae84ad381fe6cb8e26817299eccbad48c439bdf2
duxn2901 <vdb2901k55@gmail.com>
2025-11-15 21:56:46 +0100

    6

--- Lineares Gleichungsystem/LGS.java
+++ Lineares Gleichungsystem/LGS.java
@@ -1,13 +1,16 @@
+import java.util.Scanner;
+
 public class LGS {
     
     static double[][] matrix = new double[3][4];
     public static void main(String[] args) {
-        erstellen();
-        addieren(2, 0, -2);
+        // erstellen();
+        eingeben();
         loesen();
         ausgeben();
     }
 
+    //nicht mehr gebraucht
     public static void erstellen() {
         matrix[0][0] = 1;
         matrix[0][1] = 1;
@@ -47,27 +50,38 @@ public class LGS {
     }
 
     public static void loesen() {
-        //1.spalt: [1][0] und [2][0] nullen
+        //1.spalte: [1][0] und [2][0] nullen
         addieren(1, 0, -1*(matrix[1][0]/matrix[0][0]));
         addieren(2, 0, -1*(matrix[2][0]/matrix[0][0]));
 
-        //2.spalt: [2][1] nullen
+        //2.spalte: [2][1] nullen
         addieren(2, 1, -1*(matrix[2][1]/matrix[1][1]));
 
         //[2][2] 1 machen
         multiplizieren(2, 1/matrix[2][2]);
 
-        //3.spalt: [0][2] und [1][2] nullen
+        //3.spalte: [0][2] und [1][2] nullen
         addieren(0, 2, -1*(matrix[0][2]));
         addieren(1, 2, -1*(matrix[1][2]));
 
         //[1][1] 1 machen
         multiplizieren(1, 1/matrix[1][1]);
 
-        //2.spalt [0][1] nullen
+        //2.spalte [0][1] nullen
         addieren(0, 1, -1*(matrix[0][1]));
 
         //[0][0] 1 machen
         multiplizieren(0, 1/matrix[0][0]);
+    }
+
+    public static void eingeben() {
+        Scanner scan = new Scanner(System.in);
+        for (int zeile = 0; zeile < 3; zeile++) {
+            for (int spalte = 0; spalte < 4; spalte++) {
+                System.out.println("matrix[" + zeile + "][" + spalte + "] = ");
+                matrix[zeile][spalte] = scan.nextDouble();
+            }
+        }
+        scan.close();
     }
 }
