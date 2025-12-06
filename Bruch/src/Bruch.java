cf53597b8d99cc1aa8b341d75ae3f6e2c1e380a1
duxn2901 <vdb2901k55@gmail.com>
2025-12-06 13:57:52 +0100

    new GCDFinden(), kuerzen(), edited older methods to look cleaner

--- Bruch/src/Bruch.java
+++ Bruch/src/Bruch.java
@@ -7,6 +7,9 @@ public class Bruch{
         System.out.println(br1);
         Bruch br3 = br1.dividieren(new Bruch(4,6));
         System.out.println(br3);
+        Bruch br4 = new Bruch(12,2);
+        br4 = br4.kuerzen();
+        System.out.println(br4);
     }
 
 
@@ -17,28 +20,39 @@ public class Bruch{
     }
 
     public String toString() {
+        if (nenner == 1) return zaehler + "";
         return zaehler + "/" + nenner;
     }
 
     public Bruch multiplizieren(Bruch br) {
-        return new Bruch(this.zaehler*br.zaehler, this.nenner*br.nenner);
+        return new Bruch(zaehler*br.zaehler, nenner*br.nenner);
     }
 
     public Bruch kehrenBruch() {
-        return new Bruch(this.nenner, this.zaehler);
+        return new Bruch(nenner, zaehler);
     }
 
     public Bruch dividieren(Bruch br) {
         return multiplizieren(br.kehrenBruch());
     }
 
-    // public Bruch kuerzen() {
-    //     return 
-    // }
+    public Bruch kuerzen() {
+        int gcd = GCDFinden(zaehler, nenner);
+        return new Bruch(zaehler / gcd, nenner / gcd);
+    }
 
-    // public static int findenGCD(int a, int b) {
-    //     return
-    // }
+    public static int GCDFinden(int a, int b) {
+        int gcd = -1;
+        if (a==b) return a;
+        int min = (a < b)? a : b;
+        for (int i = min/2+1; i > 1; i--) {
+            if (a % i == 0 && b % i == 0) {
+                gcd = i;
+                break;
+            }
+        }
+        return gcd;
+    }
 
     // public Bruch addieren(Bruch br) {
     //     return
