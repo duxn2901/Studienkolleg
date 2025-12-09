1b0875896df24cba16976f6e2197c98bd97d1cb4
duxn2901 <vdb2901k55@gmail.com>
2025-12-09 09:50:19 +0100

    copied Bruch.java to Vektor/src/

---
+++ Vektor/src/Bruch.java
@@ -0,0 +1,82 @@
+public class Bruch{
+    public static void main(String[] args) {
+        // Bruch br1 = new Bruch(2,3);
+        // System.out.println(br1);
+        // Bruch br2 = br1.multiplizieren(new Bruch(4,6));
+        // System.out.println(br2);
+        // // System.out.println(br1);
+        // // Bruch br3 = br1.dividieren(new Bruch(4,6));
+        // // System.out.println(br3);
+        // // Bruch br4 = new Bruch(12,2);
+        // // br4 = br4.kuerzen();
+        // // System.out.println(br4);
+        // Bruch br5 = new Bruch(-5,6);
+        // Bruch br6 = new Bruch(9, 24);
+        // System.out.println(br5.subtrahieren(br6));
+    }
+
+
+    private int zaehler, nenner;
+    public Bruch(int zaehler, int nenner) {
+        this.zaehler = zaehler;
+        this.nenner = nenner;
+    }
+
+    public String toString() {
+        if (nenner == 1) return zaehler + "";
+        return zaehler + "/" + nenner;
+    }
+
+    public Bruch multiplizieren(Bruch br) {
+        return new Bruch(zaehler*br.zaehler, nenner*br.nenner).kuerzen();
+    }
+
+    public Bruch kehrenBruch() {
+        return new Bruch(nenner, zaehler);
+    }
+
+    public Bruch dividieren(Bruch br) {
+        return multiplizieren(br.kehrenBruch());
+    }
+
+    public Bruch kuerzen() {
+        int gcd = GCDFinden(zaehler, nenner);
+        return new Bruch(zaehler / gcd, nenner / gcd);
+    }
+
+    public static int GCDFinden(int a, int b) {    
+        a = Math.abs(a);
+        b = Math.abs(b);
+        while (b != 0) {
+            int temp = b;
+            b = a%b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public Bruch addieren(Bruch br) {
+        Bruch br1 = this.nennenAngleichen(br);
+        Bruch br2 = br.nennenAngleichen(br1);
+        Bruch summe = new Bruch(br1.zaehler+br2.zaehler, br1.nenner);
+        return summe.kuerzen();
+    }
+
+    public Bruch subtrahieren(Bruch br) {
+        Bruch br1 = this.nennenAngleichen(br);
+        Bruch br2 = br.nennenAngleichen(br1);
+        Bruch differenz = new Bruch(br1.zaehler-br2.zaehler, br1.nenner);
+        return differenz.kuerzen();
+    }
+
+    public Bruch nennenAngleichen(Bruch br) {
+        int lcm = LCMFinden(br.nenner, nenner);       
+        return new Bruch(zaehler * (lcm / nenner), lcm);
+    }
+
+    public int LCMFinden(int a, int b) {
+        a = Math.abs(a);
+        b = Math.abs(b);
+        return (a*b) / GCDFinden(a, b);
+    }
+}
