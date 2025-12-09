06f478e1801ff28a42754988f50ee98da11bf7bc
duxn2901 <vdb2901k55@gmail.com>
2025-12-09 09:58:26 +0100

    [ERROR] renamed addieren() to vektorAddieren(),  edited vektorAddieren() to add Bruch parameters using Bruch.addieren()

--- Vektor/src/Vektor.java
+++ Vektor/src/Vektor.java
@@ -1,7 +1,14 @@
 public class Vektor {
     public static void main(String[] args) {
-        Vektor v1 = new Vektor(new double[] {1,2,3});
-        Vektor v2 = new Vektor(new double[] {3,4,5});
+        Bruch br1 = new Bruch(1,2);
+        Bruch br2 = new Bruch(4,2);
+        Bruch br3 = new Bruch(3,5);
+        Bruch br4 = new Bruch(1,6);
+        Bruch br5 = new Bruch(5,2);
+        Bruch br6 = new Bruch(2,3);
+
+        Vektor v1 = new Vektor(new Bruch[] {br1, br2, br3});
+        Vektor v2 = new Vektor(new Bruch[] {br4, br5, br6});
         System.out.println(v1.addieren(v2));
         System.out.println(v1.skalarproduktMultiplizieren(v2));
         System.out.println(v1.faktorMultiplizieren(2));
@@ -20,9 +27,9 @@ public class Vektor {
     }
 
     public Vektor addieren(Vektor v) {
-        Vektor ergebnis = new Vektor(new double[3]);
+        Vektor ergebnis = new Vektor(new Bruch[3]);
         for (int i = 0; i < 3; i++) {
-            ergebnis.koord[i] = this.koord[i] + v.koord[i];
+            ergebnis.koord[i] = this.koord[i].addieren(v.koord[i]);
         }
         return ergebnis;
     }
